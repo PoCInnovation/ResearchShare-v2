14b4d2d61bee271a19fcb58118aaad7e99171ecf
Ersikan <julien.philippon@epitech.eu>
2021-05-16 02:58:59 +0200

    WIP: basic Permission Manager

---
+++ contracts/OwnedPermissionManager.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./PermissionManager.sol";
+
+contract OwnedPermissionManager is PermissionManager, Ownable {
+    
+    mapping(address => bool) publishers;
+    mapping(address => bool) reviewers;
+    
+    
+    function canPublish(address _address) public view override returns (bool)
+    {
+        return publishers[_address];
+    }
+    
+    function canReview(address _address) public view override returns (bool)
+    {
+        return reviewers[_address];
+    }
+    
+    function addReviewer(address _address) public onlyOwner {
+        reviewers[_address] = true;
+    }
+    
+    function addPublisher(address _address) public onlyOwner {
+        publishers[_address] = true;
+    }
+    
+    function removeReviewer(address _address) public onlyOwner {
+        reviewers[_address] = false;
+    }
+    
+    function removePublisher(address _address) public onlyOwner {
+        publishers[_address] = false;
+    }
+}

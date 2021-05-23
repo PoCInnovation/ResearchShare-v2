767e63dfb85c130a8f759e428d5b089387ae8744
Ersikan <julien.philippon@epitech.eu>
2021-05-23 13:42:40 +0200

    Separate PermissionManager and AnonymitySessionManager

--- contracts/PermissionInterface.sol
+++ contracts/PermissionInterface.sol
@@ -3,6 +3,4 @@ pragma solidity ^0.8.4;
 interface PermissionInterface {
     function canPublish(address _address) external returns (bool);
     function canReview(address _address) external returns (bool);
-    function revealIdentity(address _identity) external;
-    function getRevealedIdentity(address _address) external returns (address revealedIdentity);
 }

78d5a2459d73c292374c7214ade96243d8598851
Alexandre Monier <alexandre.monierc@gmail.com>
2021-06-08 17:58:46 +0200

    Add zokrates

---
+++ zokrates/zokrates.js
@@ -0,0 +1,25 @@
+const { initialize } = require('zokrates-js/node');
+const fs = require('fs');
+
+initialize().then((zokratesProvider) => {
+    const source = fs.readFileSync("zokrates/zokrates.zok").toString();
+
+    // compilation
+    const artifacts = zokratesProvider.compile(source);
+
+    // computation
+    const { witness, output } = zokratesProvider.computeWitness(artifacts, ["2"]);
+
+    // run setup
+    const keypair = zokratesProvider.setup(artifacts.program);
+
+    // generate proof
+    const proof = zokratesProvider.generateProof(artifacts.program, witness, keypair.pk);
+
+    // fs.writeFileSync("zokrates/zokrates.json", proof);
+    
+    // export solidity verifier
+    const verifier = zokratesProvider.exportSolidityVerifier(keypair.vk, "v1");
+
+    fs.writeFileSync("zokrates/zokrates.sol", verifier);
+});

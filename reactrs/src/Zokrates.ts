3b862877ef45d17fb927463fdc42ad82033003f7
Alexandre Monier <alexandre.monierc@gmail.com>
2021-08-03 19:52:40 +0200

    Use state instead of global

--- reactrs/src/Zokrates.ts
+++ reactrs/src/Zokrates.ts
@@ -2,14 +2,7 @@ import { initialize, ZoKratesProvider, CompilationArtifacts, ComputationResult, 
 
 export const zokrates = initialize();
 
-declare global {
-    interface Window { progress: number; statusMessage: string }
-}
-
-window.progress = 0;
-window.statusMessage = "Initialization...";
-
-export async function zokratesCompile(zokrates: ZoKratesProvider): Promise<CompilationArtifacts> {
+export async function compile(zokrates: ZoKratesProvider): Promise<CompilationArtifacts> {
     const request = await fetch("reviewer.zok");
     if (request.ok === false)
         throw new Error(request.statusText);
@@ -24,7 +17,7 @@ export async function zokratesCompile(zokrates: ZoKratesProvider): Promise<Compi
     return artifacts
 }
 
-export async function zokratesCompute(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts, reviewers: Array<string>, computeArguments: Array<string>): Promise<ComputationResult> {
+export async function compute(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts, reviewers: Array<string>, computeArguments: Array<string>): Promise<ComputationResult> {
     const reviewersHashes = []; 
     const argumentsHashes = [];
     for (const reviewer of reviewers) {
@@ -51,51 +44,17 @@ export async function zokratesCompute(zokrates: ZoKratesProvider, artifacts: Com
     return result;
 }
 
-export function zokratesKeypair(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts): SetupKeypair {
+export function keypair(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts): SetupKeypair {
     const keypair = zokrates.setup(artifacts.program);
     return keypair;
 }
 
-export function zokratesProof(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts, result: ComputationResult, keypair: SetupKeypair): Proof {
+export function proof(zokrates: ZoKratesProvider, artifacts: CompilationArtifacts, result: ComputationResult, keypair: SetupKeypair): Proof {
     const proof = zokrates.generateProof(artifacts.program, result.witness, keypair.pk);
     return proof;
 }
 
-export function zokratesVerifier(zokrates: ZoKratesProvider, keypair: SetupKeypair): string {
+export function verifier(zokrates: ZoKratesProvider, keypair: SetupKeypair): string {
     const verifier = zokrates.exportSolidityVerifier(keypair.vk, "v2");
     return verifier;
 }
-
-zokrates.then(async zokratesProvider => {
-    console.log("Compiling...");
-    window.statusMessage = "Compiling...";
-    const artifacts = await zokratesCompile(zokratesProvider);
-    window.progress = 20;
-    console.log("Done!");
-    console.log("Computing...")
-    window.statusMessage = "Computing...";
-    const result = await zokratesCompute(zokratesProvider, artifacts,
-        Array(32).fill("61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4"),
-        Array(4).fill("ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"));
-    window.progress = 40;
-    console.log("Done!");
-    console.log("Generating keypair...");
-    window.statusMessage = "Generating keypair...";
-    const keypair = zokratesKeypair(zokratesProvider, artifacts);
-    window.progress = 60;
-    console.log("Done!");
-    console.log("Generating proof...");
-    window.statusMessage = "Generating proof...";
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const proof = zokratesProof(zokratesProvider, artifacts, result, keypair);
-    window.progress = 80;
-    console.log("Done!");
-    console.log("Generating verifier...");
-    window.statusMessage = "Generating verifier...";
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const verifier = zokratesVerifier(zokratesProvider, keypair);
-    window.progress = 100;
-    window.statusMessage = "Done!";
-    console.log("Done!");
-    console.log(result);
-});

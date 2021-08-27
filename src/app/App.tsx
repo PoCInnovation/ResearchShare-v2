207a9c7797333e62c75b3c5d0133e4d5f2d303f0
Alexandre Monier <alexandre.monierc@gmail.com>
2021-08-27 21:51:40 +0200

    Update MetaMask login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,19 +8,17 @@ import Review from '../components/Review/Review';
 export default function App() {
   return (
     <div className="app">
-      <h1>Research Share</h1>
+      <h1 className="app-title">Research Share</h1>
       <div className="app-wrapper">
-          <div className="app-left">
-            <MetaMask />
-          </div>
-          <div className="app-center">
-            <IpfsHandling />
-            <br />
-            <Review />
-          </div>
-          <div className="app-right">
-
-          </div>
+        <div className="app-left">
+          <MetaMask />
+        </div>
+        <div className="app-center">
+          <IpfsHandling />
+        </div>
+        <div className="app-right">
+          <Review />
+        </div>
       </div>
     </div>
   )
--- src/components/MetaMask/MetaMask.tsx
+++ src/components/MetaMask/MetaMask.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import "./MetaMask.css";
 
-import UserContract from '../User/UserContract';
 import { useMetaMask } from 'metamask-react';
+import { Button } from '@material-ui/core';
+import { IMetaMaskContext } from 'metamask-react/lib/metamask-context';
+import UserContract from '../User/UserContract';
+
+const MetaMaskSteps = {
+    "initializing": () => <div>Synchronisation with MetaMask ongoing...</div>,
+    "unavailable": () => <div>MetaMask not available :(</div>,
+    "notConnected": ({ connect }: IMetaMaskContext) => <Button onClick={connect}>Connect to MetaMask</Button>,
+    "connecting": () => <div>Connecting...</div>,
+    "connected": ({ account }: IMetaMaskContext) => <><div>Connected account: {account}</div><br /> <UserContract /></>
+};
 
 export default function MetaMask() {
-    const { account } = useMetaMask();
+    const MetaMaskInfo = useMetaMask();
 
     return (
         <div className="metamask-register">
-            <h2>Register</h2>
-            { account ?? <UserContract/> }
+            <h2>Register/Login</h2>
+            { MetaMaskSteps[MetaMaskInfo.status](MetaMaskInfo) }
         </div>
     )
 }
